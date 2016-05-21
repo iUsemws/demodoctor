884e8f8af062064682f10d430d45159fec752ccb
michaelbahr <mbahr92@gmail.com>
2016-05-21 14:33:33 +0200

    removed messages part, structured chatForm for work with david

--- src/app/services/chatService.ts
+++ src/app/services/chatService.ts
@@ -5,18 +5,18 @@ import {UserService} from './userService';
 
 @Injectable()
 export class ChatService {
-    private _lastTimestamp: Date;
-    private _baseApi: string = 'http://localhost:9090/';
+    private _lastTimestamp:Date;
+    private _baseApi:string = 'http://139.59.152.219:3000/bot/';
 
-    constructor(private _http: Http, private _userService: UserService) {
+    constructor(private _http:Http, private _userService:UserService) {
     }
 
-    public list(): Observable<Array<string>> {
+    public list():Observable<Array<string>> {
         const userId = this._userService.id;
 
         const params = new URLSearchParams();
         const lastTimestamp = this._lastTimestamp;
-        
+
         if (lastTimestamp) {
             params.set('timestamp', <string>lastTimestamp.getTime());
         }
@@ -34,16 +34,16 @@ export class ChatService {
                 this._lastTimestamp = new Date();
             });
     }
-    
-    public post(message): Observable<Response> {
+
+    public post(message):Observable<Response> {
         const userId = this._userService.id;
-        
+
         return this._http.post(`${this._baseApi}message/${userId}`, JSON.stringify(message), {
             headers: this.createHeaders()
         });
     }
 
-    private createHeaders(): Headers {
+    private createHeaders():Headers {
         const headers = new Headers();
         headers.append('Content-Type', 'application/json');
         return headers;
